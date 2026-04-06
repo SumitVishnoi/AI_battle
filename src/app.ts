0ba298ea3ebccaa52642ec95ade68b7b90bd2dfb
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-04-06 23:07:43 +0530

    add AI model integration and evaluation logic to graph

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from "express"
+import runGraph from "./ai/graph.ai.js"
 
 const app = express()
+app.use(express.json())
 
+app.get("/", async (req, res)=> {
+    const result = await runGraph("Write an code for Factorial function in js")
+
+    res.json(result)
+})
+
+app.post("/invoke", async (req, res)=> {
+    const {input} = req.body
+
+    const result = await runGraph(input)
+
+    res.status(200).json({
+        message: "Graph executed successfully",
+        success: true,
+        result
+    })
+})
 
 export default app
